ea83636b79b97b146cc208ff74df6c36aae51cef
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2017-05-24 13:53:36 +0300

    Сделал получение секунд для всех платформ

---
+++ WeatherDemo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonGameObject<GameManager>
+{
+
+    private float time = 0.0f;
+    private int timeInSeconds = 0;
+
+    bool isPaused = false;
+
+    private bool isCountingTime = false;
+
+	// Use this for initialization
+	void Start () {
+		
+        StartTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (isCountingTime)
+	    {
+            time += Time.deltaTime;
+        }
+	}
+
+    void OnDestroy()
+    {
+        
+    }
+
+    void OnApplicationQuit()
+    {
+        StopTimer();
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        if (pauseStatus)
+        {
+            StopTimer();
+        }
+        else
+        {
+            StartTimer();
+        }
+    }
+
+    void OnApplicationFocus(bool hasFocus)
+    {
+        if (!hasFocus)
+        {
+            StopTimer();
+        }
+        else
+        {
+            StartTimer();
+        }
+    }
+
+    private void StartTimer()
+    {
+        time = 0;
+        timeInSeconds = 0;
+        isCountingTime = true;
+    }
+
+    private void StopTimer()
+    {
+        isCountingTime = false;
+
+        if (time > 0)
+        {
+            timeInSeconds = (int) time % 60;
+            Debug.Log("timeInSeconds " + timeInSeconds);
+        }
+        else
+        {
+            Debug.LogError("time is 0");
+        }
+    }
+
+
+}
---
+++ WeatherDemo/Assets/Scripts/SingletonGameObject.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Singletone GameObject.
+/// </summary>
+public class SingletonGameObject<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                T[] instances = FindObjectsOfType(typeof(T)) as T[];
+
+                if (instances.Length == 0)
+                {
+                    Debug.LogError("An instance of " + typeof(T) +
+                    " is needed in the scene, but there is none.");
+                }
+                else if (instances.Length > 1)
+                {
+                    Debug.LogError("There is more than one instance of " + typeof(T) +
+                    " in the scene.");
+                }
+                else
+                {
+                    instance = instances[0];
+                    Debug.LogWarning("Something called instance of " + typeof(T) +
+                    " before it was initialized.");
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null || instance == this)
+        {
+            instance = this as T;
+            Debug.Log("Instance of " + typeof(T) + " created in the scene.");
+        }
+        else
+        {
+            Debug.LogError("Second instance of " + typeof(T) + " created in the scene.");
+        }
+    }
+}
